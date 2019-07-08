5f3f37f976de12fec9e7da2734f719c200f43bdf
Phil <philipp.butz92@gmail.com>
2019-07-08 23:14:53 +0200

    added generic confirmation modal (to be implemented)

--- components-reactor/gui/src/app/app.module.ts
+++ components-reactor/gui/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmationModalComponent } from './shared/confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ConfirmationModalComponent
   ],
   imports: [
     BrowserModule,
