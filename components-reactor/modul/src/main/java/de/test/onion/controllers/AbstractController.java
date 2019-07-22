5afbea26b93dc11ac8ecf71066f0bf38089bd859
Philly-B <philipp.butz@averbis.com>
2019-07-22 11:25:40 +0200

    allow cors requests from ng serve port

--- components-reactor/modul/src/main/java/de/test/onion/controllers/AbstractController.java
+++ components-reactor/modul/src/main/java/de/test/onion/controllers/AbstractController.java
@@ -1,6 +1,7 @@
 package de.test.onion.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +11,7 @@ import de.test.onion.domain.exceptions.ObjectDoesNotExistException;
 import de.test.onion.dtos.ErrorResponseDto;
 import de.test.onion.exceptions.IllegalIdException;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public abstract class AbstractController {
 
 	@ExceptionHandler(value = IllegalIdException.class)
